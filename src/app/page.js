923d36c8411804ba2506a811f90d43a935651aac
Abdisalamabdi303 <152012883+Abdisalamabdi303@users.noreply.github.com>
2024-11-18 13:27:19 +0300

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
 import Results from '@/components/Results';
-
+import { useSearchParams } from 'next/navigation'
 const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
 
-export default async function Home({ searchParams }) {
-  const genre = searchParams.genre
+export default async function Home() {
+  const searchParams= useSearchParams()
+  const genre = searchParams.get('genre')
   const res = await fetch(
     `https://api.themoviedb.org/3${
       genre === 'fetchTopRated' ? `/movie/top_rated` : `/trending/all/week`
