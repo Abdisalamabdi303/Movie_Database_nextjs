c29fa0414f407af3b72bdb1b2a3cf64b169da1ac
Abdisalamabdi303 <152012883+Abdisalamabdi303@users.noreply.github.com>
2024-11-23 12:41:01 +0300

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
 
 export default async function Home({searchParams}) {
 
-const genre = searchParams.get('genre')
+const genre = (await searchParams).get('genre')
   const res = await fetch(
     `https://api.themoviedb.org/3${
       genre === 'fetchTopRated' ? `/movie/top_rated` : `/trending/all/week`
